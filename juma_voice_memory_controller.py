8a4685ca007f5681f918887093d93f640583a0c6
extrememan-fin <am.mustonen@gmail.com>
2025-10-26 12:30:01 +0200

    Update juma_voice_memory_controller.py

--- juma_voice_memory_controller.py
+++ juma_voice_memory_controller.py
@@ -7,7 +7,7 @@ Cross-platform RS-232 controller for JUMA transceivers.
 Provides a compact, intuitive CustomTkinter interface for controlling
 voice memory functions such as MIC record, RX record, Play, Transmit, and Stop.
 
-Developed by OH2DDG with technical assistance from OH7SV.
+Developed by extrememan-fin OH2DDG with technical assistance from OH7SV.
 
 """
 
